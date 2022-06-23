d8662a3f8560088a473e5f4725b2f45041e85d87
quan <304390323@qq.com>
2022-06-24 03:15:47 +0800

    init

---
+++ dist/index.ae981c64.js
@@ -0,0 +1,2 @@
+window.dom={find:(e,l)=>(l||document).querySelectorAll(e),style(e,l,t){if(3===arguments.length)e.style[l]=t;else if(2===arguments.length){if("string"==typeof l)return e.style[l];if(l instanceof Object){const t=l;for(let l in t)e.style[l]=t[l]}}},each(e,l){for(let t=0;t<e.length;t++)l.call(null,e[t])}};
+//# sourceMappingURL=index.ae981c64.js.map
---
+++ dist/index.bc458c2c.js
@@ -0,0 +1,2 @@
+const div=dom.find("#test>.red")[0];console.log(div),dom.style(div,"color","green");const divList=dom.find(".red");dom.each(divList,(o=>console.log(o)));
+//# sourceMappingURL=index.bc458c2c.js.map
