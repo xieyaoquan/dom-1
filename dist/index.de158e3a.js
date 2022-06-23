b763a13a27fe5fbc032e0be57f3b4ea3229df02a
quan <304390323@qq.com>
2022-06-24 01:08:43 +0800

    .

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,7 @@
+const div = dom.find("#test>.red")[0];
+dom.style(div, "color", "green");
+const divList = dom.find(".red");
+dom.each(divList, (n)=>console.log(n)
+);
+
+//# sourceMappingURL=index.de158e3a.js.map
---
+++ src/main.js
@@ -0,0 +1,4 @@
+const div = dom.find("#test>.red")[0];
+dom.style(div, "color", "green");
+const divList = dom.find(".red");
+dom.each(divList, (n) => console.log(n));
